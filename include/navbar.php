95e7c14c56813e0755bab7da81ffb343db48f6db
Anas <92400545+Rukilaxy@users.noreply.github.com>
2022-05-10 17:57:28 +0100

    Add files via upload

---
+++ include/navbar.php
@@ -0,0 +1,18 @@
+<div class="navbar navbar-expand navbar-dark bg-primary text-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="ajouter.php">Ajouter</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+</div>
